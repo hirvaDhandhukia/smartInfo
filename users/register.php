c4bf93eb20e1085de93b5a548c5b4323e819cb2c
maitrakhatri <maitrakhatri@gmail.com>
2021-08-03 15:20:31 +0530

    updated registration form
    moved it above the email

--- users/register.php
+++ users/register.php
@@ -86,6 +86,16 @@
   </div>
 
   <div class="form-group">
+    <label for="validationCustom03">Aadhar Number</label>
+    <input type="" class="form-control" id="validationCustom03"  pattern="\d*" minlength="10" maxlength="10" required>
+    <div class="invalid-feedback"> Enter 10 digit Aadhar Number</div>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+
+
+  <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
     <div class="invalid-feedback"> Enter a valid Email Id</div>
@@ -111,18 +121,8 @@
     </div>
   </div>
   
-  <div class="form-group">
-    <label for="validationCustom03">Aadhar Number</label>
-    <input type="" class="form-control" id="validationCustom03"  pattern="\d*" minlength="10" maxlength="10" required>
-    <div class="invalid-feedback"> Enter 10 digit Aadhar Number</div>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
 
-
-
-<button class="btn btn-primary" type="submit">Submit form</button>
+<button class="btn btn-primary" type="submit">Register</button>
 </form>
 
 <script>
