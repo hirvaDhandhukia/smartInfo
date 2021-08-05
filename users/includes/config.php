547996f172e0ddb53b5e7f79329ce345310bd5bc
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2021-08-05 21:31:39 +0530

    add: database connection

---
+++ users/includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Database connection here
+$dbServerName = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'smartinfo';
+
+// this '$conn' is a variable that connects to database
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn) {
+	die('Error connecting to database');
+}
--- users/index.php
+++ users/index.php
@@ -1,9 +1,3 @@
-<?php
-// the 'users' folder has all the files for patients
-// index.php file is patient-homepage
-// start ur html after the closing php tags
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
