99bacdf89dbd4b9b6f6a7aee844f4628bbbe598d
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2021-08-09 00:37:15 +0530

    add: error messages

--- users/register.php
+++ users/register.php
@@ -45,6 +45,35 @@
   </div>
   </nav>
 
+  <?php
+  if(isset($_GET['error'])) {
+    if($_GET['error'] == "emptyinput") {
+      echo "<p>Fill in all fields</p>";
+    }
+    else if ($_GET['error'] == "invalidaadhar") {
+      echo "<p>Enter a correct aadhar number</p>";
+    }
+    else if ($_GET['error'] == "invalidemail") {
+      echo "<p>Fill in correct email</p>";
+    }
+    else if ($_GET['error'] == "passwordsdontmatch") {
+      echo "<p>Passwords doesn't match</p>";
+    }
+    else if ($_GET['error'] == "stmtfailed") {
+      echo "<p>Something went wrong. Please try again.</p>";
+    }
+    else if ($_GET['error'] == "stmtfailedcreateuser") {
+      echo "<p>Something went wrong. Please try again.</p>";
+    }
+    else if ($_GET['error'] == "emailtaken") {
+      echo "<p>Email or Aadhar number already taken, try anything else.</p>";
+    }
+    else if ($_GET['error'] == "none") {
+      echo "<p>Registration Success!</p>";
+    }
+  }
+?>
+
 
 
     <br>
