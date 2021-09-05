06a0e5991202556870054c34683f5e5ba0269836
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2021-09-05 07:19:33 +0530

    add: backend admin register

---
+++ admin/includes/register-admin.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+if(isset($_POST["submit"])) {
+
+	// grabbing the data from the url
+	$fname = $_POST["fname"];
+	$lname = $_POST["lname"];
+	$email = $_POST["email"];
+	$aadharno = $_POST["aadharno"];
+	$dob = $_POST["dob"];
+	$regno = $_POST["regno"];
+	$speciality = $_POST["speciality"];
+	$password = $_POST["password"];
+	$cpassword = $_POST["cpassword"];
+
+	// error handling
+	require_once 'config.php';
+	require_once 'functions.inc.php';
+
+	if(emptyInputRegister($fname, $lname, $email, $aadharno, $dob, $regno, $speciality, $password, $cpassword) !== false) {
+		header("location: ../register-admin.php?error=emptyinput");
+		exit();
+	}
+	if(invalidAadhar($aadharno) !== false) {
+		header("location: ../register-admin.php?error=invalidaadhar");
+		exit();
+	}
+	if(invalidEmail($email) !== false) {
+		header("location: ../register-admin.php?error=invalidemail");
+		exit();
+	}
+	if(pwdMatch($password, $cpassword) !== false) {
+		header("location: ../register-admin.php?error=passwordsdontmatch");
+		exit();
+	}
+	if(emailExists($conn, $email, $aadharno, $regno) !== false) {
+		header("location: ../register-admin.php?error=emailtaken");
+		exit();
+	}
+
+	// if we are here now then, user made no mistakes
+	// so we can signup the user into our website
+	createAdmin($conn, $fname, $lname, $email, $aadharno, $dob, $regno, $speciality, $password);
+
+} else {
+	header("location: ../register-admin.php");
+		exit();
+}
